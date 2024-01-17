be1466df4c0dd9ff69949ba89f955baf7d35cbb0
N1K1111 <nik.glazkov2016@yandex.ru>
2024-01-17 17:20:13 +0300

    Добавьте файлы проекта.

---
+++ Producer/Producers/Producer.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Producer.Producers
+{
+    public class Producer
+    {
+        private string _exchangeType;
+        private string _exchangeName;
+        private string _routingKey;
+        private IModel _channel;
+
+        public Producer(string exchangeType, string exchangeName,string routingKey,IModel channel)
+        {
+            _exchangeType = exchangeType;
+            _exchangeName = exchangeName;
+            _routingKey = routingKey;
+            _channel = channel;
+            _channel.ExchangeDeclare(_exchangeName, _exchangeName);
+        }
+
+        public void Produce(string messageContent)
+        {
+            var message = new MessageDto()
+            {
+                Content = $"{messageContent} exchange: {_exchangeType}"
+            };
+
+            var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+            _channel.BasicPublish(_exchangeName,
+                _routingKey,
+                basicProperties: null,
+                body: body);
+
+            Console.WriteLine($"Сообщение отправленно в обменник: {_exchangeName}");
+
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Producer.Settings;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json",true,true)
+                .Build();
+
+            var connection = GetRabbitConnection(configuration);
+            var channel = connection.CreateModel();
+
+            var producer = new Producers.Producer("direct", "exchange.direct", "key_1", channel);
+            //var producer = new Producers.Producer("fanout", "exchange.fanout", "key_1", channel);
+            //var producer = new Producers.Producer("topic", "exchange.topic", "key_1", channel);
+        
+            /*
+            while (true)
+            {
+                producer.Produce("Message");
+                Thread.Sleep(1000);
+            }
+            */
+
+            //channel.Close();
+            //connection.Close();
+        }
+
+        private static IConnection GetRabbitConnection(IConfiguration configuration)
+        {
+            var rmqSettings = configuration.Get<AppSettings>().RmqSettings;
+
+            ConnectionFactory connectionFactory = new ConnectionFactory 
+            {
+                HostName = rmqSettings.Host,
+                VirtualHost = rmqSettings.VHost,
+                UserName = rmqSettings.Login,
+                Password = rmqSettings.Password,
+            };
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+}
